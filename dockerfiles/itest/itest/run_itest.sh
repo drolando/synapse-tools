867d337fba1d9e13e44dca9e419e025153973078
Joseph Lynch <joe.e.lynch@gmail.com>
2016-01-07 12:56:09 -0800

    Use c pyyaml extensions when present
    The native python yaml parser is _really_ slow, so we should build
    against the libyaml-dev package so that we can take advantage of c
    bindings if we have them

---
+++ dockerfiles/itest/itest/run_itest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Installing synapse-tools package"
+dpkg -i /work/dist/synapse-tools_*.deb
+
+# Set -e here because the previous install should fail lacking dependencies
+# and then we can fix it here
+set -e
+apt-get -y install -f
+
+echo "Testing that pyyaml uses optimized cyaml parsers if present"
+/usr/share/python/synapse-tools/bin/python -c 'import yaml; assert yaml.__with_libyaml__'
+
+echo "Full integration test"
+py.test /itest.py
