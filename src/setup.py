e493ead34741c3a9ae2caedd5c1de4aea7181a8b
Evan Krall <krall@yelp.com>
2016-05-17 20:47:24 -0700

    Bump to version 0.10.1

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='synapse-tools',
-    version='0.10.0',
+    version='0.10.1',
     provides=['synapse_tools'],
     author='John Billings',
     author_email='billings@yelp.com',
