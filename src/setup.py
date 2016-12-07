469ca38c7ae07aee4866123298ff3b448a780a85
Matthew Smith <matts@yelp.com>
2016-12-07 14:09:42 -0800

    bumped synapse-tools to 0.11.2

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='synapse-tools',
-    version='0.11.0',
+    version='0.11.2',
     provides=['synapse_tools'],
     author='John Billings',
     author_email='billings@yelp.com',
