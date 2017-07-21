drolando/synapse-tools

commit 316010e3b5ac1e93082201bac30afaf39c19e109

Build for xenial

Makefile
@@ -1,30 +1,17 @@
-all: itest_trusty
-
-itest_trusty: package_trusty bintray.json
-	rm -rf dockerfiles/itest/itest_trusty
-	cp -a dockerfiles/itest/itest dockerfiles/itest/itest_trusty
-	cp dockerfiles/itest/itest/Dockerfile.trusty dockerfiles/itest/itest_trusty/Dockerfile
-	tox -e itest_trusty
-
 DATE := $(shell date +'%Y-%m-%d')
 SYNAPSETOOLSVERSION := $(shell sed 's/.*(\(.*\)).*/\1/;q' src/debian/changelog)
 bintray.json: bintray.json.in src/debian/changelog
 	sed -e 's/@DATE@/$(DATE)/g' -e 's/@SYNAPSETOOLSVERSION@/$(SYNAPSETOOLSVERSION)/g' $< > $@
 
-package_trusty:
-	[ -d dist ] || mkdir dist
-	tox -e package_trusty
+itest_%: package_%
+	rm -rf dockerfiles/itest/itest_$*
+	cp -a dockerfiles/itest/itest dockerfiles/itest/itest_$*
+	cp dockerfiles/itest/itest/Dockerfile.$* dockerfiles/itest/itest_$*/Dockerfile
+	tox -e itest_$*
 
-# Note: itest_lucid will not build outside of Yelp - it depends on python2.7, which is not generally available for lucid anymore.
-itest_lucid: package_lucid
-	rm -rf dockerfiles/itest/itest_lucid
-	cp -a dockerfiles/itest/itest dockerfiles/itest/itest_lucid
-	cp dockerfiles/itest/itest/Dockerfile.lucid dockerfiles/itest/itest_lucid/Dockerfile
-	tox -e itest_lucid
-
-package_lucid:
+package_%:
 	[ -d dist ] || mkdir dist
-	tox -e package_lucid
+	tox -e package_$*
 
 clean:
 	tox -e fix_permissions

.gitignore
@@ -9,6 +9,7 @@ synapse-tools*.dsc
 synapse-tools*.tar.gz
 src/.eggs
 src/build
+src/debian/debhelper-build-stamp
 src/debian/files
 src/debian/synapse-tools
 src/debian/synapse-tools.debhelper.log
@@ -16,6 +17,7 @@ src/debian/synapse-tools.substvars
 src/synapse_tools.egg-info
 dockerfiles/itest/itest_lucid/
 dockerfiles/itest/itest_trusty/
+dockerfiles/itest/itest_xenial/
 src/debian/synapse-tools.postinst.debhelper
 src/.cache/
 bintray.json

dockerfiles/itest/fig_xenial.yml
@@ -1,0 +1,26 @@
+itest:
+  build: dockerfiles/itest/itest_xenial
+  hostname: itesthost.itestdomain
+  volumes:
+   - .:/work
+  links:
+   - servicetwo
+   - servicethree
+   - servicethreechaos
+   - serviceone
+   - zookeeper
+
+servicethree:
+  build: dockerfiles/itest/service_three
+
+serviceone:
+  build: dockerfiles/itest/service_one
+
+zookeeper:
+  build: dockerfiles/itest/zookeeper
+
+servicethreechaos:
+  build: dockerfiles/itest/service_three
+
+servicetwo:
+  build: dockerfiles/itest/service_two

dockerfiles/itest/itest/Dockerfile.xenial
@@ -1,0 +1,45 @@
+FROM ubuntu:xenial
+MAINTAINER John Billings <billings@yelp.com>
+
+RUN apt-get update && apt-get -y install \
+	libcurl3 \
+	iptables \
+	python-setuptools \
+	python-pytest \
+	python-pycurl \
+	python-kazoo \
+	python-zope.interface \
+	python2.7 \
+    ruby \
+    ruby-dev \
+    libxml2 \
+    libxml2-dev \
+    libxslt-dev \
+    build-essential \
+    zlib1g-dev
+
+ADD https://github.com/haproxy/haproxy/archive/v1.6.0.tar.gz /haproxy.tar.gz
+RUN tar -axvf /haproxy.tar.gz
+WORKDIR /haproxy-1.6.0
+RUN make TARGET=linux26 && mv haproxy /usr/bin/haproxy-synapse
+
+# Pin for test reproducibility
+RUN gem install --no-ri --no-rdoc nokogiri -v 1.6.7.2
+RUN gem install --no-ri --no-rdoc synapse -v 0.13.8
+
+ADD synapse.conf /etc/init/synapse.conf
+ADD synapse.conf.json /etc/synapse/synapse.conf.json
+ADD synapse-tools.conf.json /etc/synapse/synapse-tools.conf.json
+ADD yelpsoa-configs /nail/etc/services
+ADD zookeeper_discovery /nail/etc/zookeeper_discovery
+ADD habitat /nail/etc/habitat
+ADD ecosystem /nail/etc/ecosystem
+ADD region /nail/etc/region
+ADD itest.py /itest.py
+ADD run_itest.sh /run_itest.sh
+
+# configure_synapse tries to restart synapse.
+# make it think it succeeded.
+RUN ln -sf /bin/true /usr/sbin/service
+
+CMD /bin/bash /run_itest.sh

dockerfiles/xenial/Dockerfile
@@ -1,0 +1,18 @@
+FROM ubuntu:xenial
+MAINTAINER John Billings <billings@yelp.com>
+
+RUN apt-get update && apt-get -y install \
+    debhelper \
+    dpkg-dev \
+    libyaml-dev \
+    libcurl4-openssl-dev \
+    python-dev \
+    python-tox \
+    python-setuptools \
+    libffi-dev \
+    libssl-dev \
+    build-essential \
+    dh-virtualenv \
+    protobuf-compiler
+
+WORKDIR /work

dockerfiles/xenial/fig.yml
@@ -1,0 +1,5 @@
+xenial:
+  build: dockerfiles/xenial
+  command: bash -c "cd src && tox -exenial && dpkg-buildpackage -d -uc -us && mv ../*.deb ../dist/"
+  volumes:
+   - .:/work

src/debian/control
@@ -3,7 +3,7 @@ Maintainer: John Billings <billings@yelp.com>
 Build-Depends:
     dh-virtualenv,
 
-Depends: python2.7, ${shlibs:Depends}
+Depends: python2.7, sudo, ${shlibs:Depends}
 Package: synapse-tools
 Architecture: any
 Description: Synapse-related tools for use on Yelp machines.

src/debian/rules
@@ -7,8 +7,8 @@ export DH_OPTIONS
 
 LSBDISTRELEASE := $(shell lsb_release -sr)
 
-ifeq ($(LSBDISTRELEASE),14.04)
-  # wheels built on Lucid won't work for Trusty
+# https://github.com/spotify/dh-virtualenv/issues/150
+ifneq ($(LSBDISTRELEASE),10.04)
   pip_args=--extra-pip-arg='--no-use-wheel'
 else
   pip_args=

commit ab7948338669cadf188bffeb81f0db981352a069

Fix deploy to bintray, use tags, add README badge
The deploy has been broken since 316010e3, when bintray.json was
removed from the Makefile, so this adds that functionality back
and does per-distribution deploys like done with paasta-tools.

This also switches to use tags for deploys, since we do actually tag
releases now (originally changed to master branch in 295c4d7e)

.travis.yml
@@ -18,5 +18,5 @@ deploy:
     key:
       secure: "F9DHZln0Zp1/A/ZvhXyn7ff55WAo8Df2NuS1/TZshXXbWpc5LXbxFrPHtoP5gBG+BuH7MnYzm4g0S1lECxbSs+fuiBE4C3nfnk8hXfl31o451WQQnjOhtJ0BHXoA2LqTd9gFFZETTGz6IGioP7sc0LrzJ7XR2iMaPSBGraZTvUQM5hodKIQ3Nfv2qbSbPyV4zAe54ukO4rn4lxqgTeDKjcbpU797QWokuePfvqa2wKShrtsWqW/mJ3vDKB3aimGPXC8TRXp1tEdF0jJIfX4nLO3HEnOMQgwRGu1Dk2/BlQvL+k8ZUEt2iNhkkh/ZpBIFmWC/vhj40zEAk3uPs3JTavULvCHQ81/UMJjA+fBve9INp93kvjC1udJCMn2FNF7fVj8xeVcSq7XIFzk/Buqn40b9AaMX80iow+8hJ68vm/r6U8U/K19azgmKCQWDYYtCmLmuNRi6TTQvF7ToyfQxNrgq8MNp6akCA7L0lad0IWhm/haV5wvOpem5ozuRwZgf8Yh9aryLt3NngndqEw51RC4onqyw1ytEtqCh1UDrJgzRJZuJozz0s3Yv7OZc09w9i4aRji6Tzd2mrfpKrjBJzIUFOAist1W0wzNsJu1flxAjh6pYvYp6wYeMxU1zsz808/c4cTbUHpmJYONmgzn/+zLqQiAXDIX5Ye1KgEE1bsI="
     on:
-      branch: master
+      tags: true
       repo: Yelp/synapse-tools

Makefile
@@ -1,9 +1,12 @@
 DATE := $(shell date +'%Y-%m-%d')
 SYNAPSETOOLSVERSION := $(shell sed 's/.*(\(.*\)).*/\1/;q' src/debian/changelog)
-bintray.json: bintray.json.in src/debian/changelog
-	sed -e 's/@DATE@/$(DATE)/g' -e 's/@SYNAPSETOOLSVERSION@/$(SYNAPSETOOLSVERSION)/g' $< > $@
+bintray_%: bintray.json.in src/debian/changelog
+	sed -e 's/@DATE@/$(DATE)/g' \
+	    -e 's/@SYNAPSETOOLSVERSION@/$(SYNAPSETOOLSVERSION)/g' \
+	    -e 's/@DISTRIBUTION@/$*/g' \
+            bintray.json.in > bintray.json
 
-itest_%: package_%
+itest_%: package_% bintray_%
 	rm -rf dockerfiles/itest/itest_$*
 	cp -a dockerfiles/itest/itest dockerfiles/itest/itest_$*
 	cp dockerfiles/itest/itest/Dockerfile.$* dockerfiles/itest/itest_$*/Dockerfile

bintray.json.in
@@ -27,7 +27,15 @@
 
     "files":
         [
-        {"includePattern": "dist/(synapse-tools.*\.deb)", "uploadPattern": "$1", "matrixParams": { "deb_distribution": "trusty", "deb_component": "main", "deb_architecture": "amd64"} }
+            {
+                "includePattern": "dist/(synapse-tools.*\.deb)",
+                "uploadPattern": "$1",
+                "matrixParams": {
+                    "deb_distribution": "@DISTRIBUTION@",
+                    "deb_component": "main",
+                    "deb_architecture": "amd64"
+                }
+            }
         ],
     "publish": true
 }