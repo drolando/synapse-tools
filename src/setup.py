ceb8447f02107ddd46119c43ca74e5fac59d4efa
Jason Perrin <jvperrin@yelp.com>
2017-07-21 11:56:53 -0700

    Bump version to 0.13.2

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='synapse-tools',
-    version='0.13.1',
+    version='0.13.2',
     provides=['synapse_tools'],
     author='John Billings',
     author_email='billings@yelp.com',
