45900aecaee020b86cd5a76ae11dc4ffa08e7962
Joseph Lynch <jlynch@yelp.com>
2017-08-16 15:46:54 -0700

    Bump to 0.13.10

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='synapse-tools',
-    version='0.13.9',
+    version='0.13.10',
     provides=['synapse_tools'],
     author='John Billings',
     author_email='billings@yelp.com',
